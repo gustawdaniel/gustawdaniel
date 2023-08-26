da1b76f55d289ec5e9a4c81dda559251f6019eaa
Daniel Gustaw <gustaw.daniel@gmail.com>
2023-08-26 23:45:13 +0400

    lang reverted, new cactus tempate installed

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,32 +1,33 @@
+// TODO:NEW
 export const languages = {
-	en: "English",
-	pl: "Polish",
-	es: "Spanish",
-	de: "German",
-	ru: "Russian",
+  en: "English",
+  pl: "Polish",
+  es: "Spanish",
+  de: "German",
+  ru: "Russian",
 };
 
 export const defaultLang = "en";
 
 export const ui = {
-	en: {
-		"nav.home": "Home",
-		"nav.about": "About",
-	},
-	pl: {
-		"nav.home": "Strona główna",
-		"nav.about": "O nas",
-	},
-	es: {
-		"nav.home": "Inicio",
-		"nav.about": "Acerca de",
-	},
-	de: {
-		"nav.home": "Startseite",
-		"nav.about": "Über uns",
-	},
-	ru: {
-		"nav.home": "Главная",
-		"nav.about": "О нас",
-	},
+  en: {
+    "nav.home": "Home",
+    "nav.about": "About",
+  },
+  pl: {
+    "nav.home": "Strona główna",
+    "nav.about": "O nas",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.about": "Acerca de",
+  },
+  de: {
+    "nav.home": "Startseite",
+    "nav.about": "Über uns",
+  },
+  ru: {
+    "nav.home": "Главная",
+    "nav.about": "О нас",
+  },
 } as const;
