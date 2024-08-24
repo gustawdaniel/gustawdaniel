c2ba6c37175f991466289070de98097171e94ee5
Daniel Gustaw <gustaw.daniel@gmail.com>
2024-08-24 17:33:14 +0400

    added lang versions of blog posts

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,27 +1,15 @@
-import {getCollection} from "astro:content";
-import {locales} from "../locales.ts";
+import { getCollection } from "astro:content";
+import { locales } from "../locales.ts";
 
 export async function GET() {
     const siteUrl = import.meta.env.SITE;
     const posts = await getCollection('blog');
 
-    const result = `  
-<?xml version="1.0" encoding="UTF-8"?>  
-
-  `.trim();
-
-    // <urlset
-    //     xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-    // xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9 http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd
-    // http://www.w3.org/1999/xhtml http://www.w3.org/2002/08/xhtml/xhtml1-strict.xsd"
-    //     xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-    // xmlns:xhtml="http://www.w3.org/1999/xhtml"
-    //     >
-
     return new Response(
         `<?xml version="1.0" encoding="UTF-8"?>
 <?xml-stylesheet type="text/xsl" href="/sitemap.xsl"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">  
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+        xmlns:xhtml="http://www.w3.org/1999/xhtml">  
   <url>
     <loc>${siteUrl}/</loc>
       ${locales.map(locale => `<xhtml:link
@@ -33,7 +21,18 @@ export async function GET() {
   ${posts
             .map((post) => {
                 const lastMod = (post.data.publishDate).toISOString();
-                return `<url><loc>${siteUrl}/posts/${post.slug}/</loc><lastmod>${lastMod}</lastmod></url>`;
+                const translatedPosts = posts.filter(p => p.data.canonicalName === post.data.canonicalName)
+
+                const alternateLinks = translatedPosts.map(p => {
+                    const locale = p.slug.split('/')[0];
+                    return `<xhtml:link rel="alternate" hreflang="${locale}" href="${siteUrl}/posts/${p.slug}" />`;
+                }).join('\n');
+
+                return `<url>
+  <loc>${siteUrl}/posts/${post.slug}</loc>
+  <lastmod>${lastMod}</lastmod>
+  ${alternateLinks}
+</url>`;
             })
             .join('\n')}  
 </urlset>`.trim(), {
